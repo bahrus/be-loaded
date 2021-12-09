f04a1a1b0c3d86dc46eae34fb5390e09b284a6bf
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-12-09 13:29:08 -0500

    0.0.4

--- be-loaded.js
+++ be-loaded.js
@@ -1,6 +1,9 @@
 "use strict";
 //https://davidwalsh.name/async-function-class
 const AsyncFunction = Object.getPrototypeOf(async function () { }).constructor;
-const doImport2 = new AsyncFunction('return await import("htps://unpkg.com/be-loaded/test.css", {assert: {type: "css"}});');
-const style = await doImport2();
-console.log(style);
+const doImport2 = (new AsyncFunction('return await import("https://unpkg.com/be-loaded/test.css", {assert: {type: "css"}});'));
+async function doImport() {
+    const style = await doImport2();
+    console.log(style);
+}
+doImport();
--- be-loaded.ts
+++ be-loaded.ts
@@ -2,8 +2,13 @@
 //https://davidwalsh.name/async-function-class
 
 const AsyncFunction = Object.getPrototypeOf(async function(){}).constructor;
-const doImport2 = new AsyncFunction('return await import("htps://unpkg.com/be-loaded/test.css", {assert: {type: "css"}});');
+const doImport2 = (new AsyncFunction('return await import("https://unpkg.com/be-loaded/test.css", {assert: {type: "css"}});')) as () => Promise<StyleSheet>;
 
-const style = await doImport2();
-console.log(style);
+async function doImport(){
+    const style = await doImport2();
+    console.log(style);
+}
+
+doImport();
+
 
