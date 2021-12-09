25fb990258e1aab81fff27665abd8b36c9ec4f8a
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-12-09 13:24:21 -0500

    0.0.4

--- be-loaded.js
+++ be-loaded.js
@@ -1,6 +1,6 @@
 "use strict";
 //https://davidwalsh.name/async-function-class
 const AsyncFunction = Object.getPrototypeOf(async function () { }).constructor;
-const doImport2 = new AsyncFunction('return await import("./test.css", {assert: {type: "css"}});');
+const doImport2 = new AsyncFunction('return await import("htps://unpkg.com/be-loaded/test.css", {assert: {type: "css"}});');
 const style = await doImport2();
 console.log(style);
--- be-loaded.ts
+++ be-loaded.ts
@@ -2,7 +2,7 @@
 //https://davidwalsh.name/async-function-class
 
 const AsyncFunction = Object.getPrototypeOf(async function(){}).constructor;
-const doImport2 = new AsyncFunction('return await import("./test.css", {assert: {type: "css"}});');
+const doImport2 = new AsyncFunction('return await import("htps://unpkg.com/be-loaded/test.css", {assert: {type: "css"}});');
 
 const style = await doImport2();
 console.log(style);
