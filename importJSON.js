834b5d53a0625d444f071a9b51745ae041094c33
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-12-18 10:31:34 -0500

    0.0.22

--- importJSON.js
+++ importJSON.js
@@ -1,7 +1,12 @@
 //https://davidwalsh.name/async-function-class
 const AsyncFunction = Object.getPrototypeOf(async function () { }).constructor;
 const doImport = (new AsyncFunction('path', 'return await import(path, {assert: {type: "json"}});'));
-export async function importJSON(url) {
+export async function importJSON(preloadRef, fallbackUrl) {
+    const preload = self[preloadRef];
+    let url = fallbackUrl;
+    if (preload !== undefined) {
+        url = preload.href;
+    }
     if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
         return await doImport(url);
     }
--- importJSON.ts
+++ importJSON.ts
@@ -2,7 +2,12 @@
 const AsyncFunction = Object.getPrototypeOf(async function(){}).constructor;
 const doImport = (new AsyncFunction('path', 'return await import(path, {assert: {type: "json"}});')) as (url: string) => Promise<any>;
 
-export async function importJSON(url: string) {
+export async function importJSON(preloadRef: string, fallbackUrl: string) {
+    const preload = (<any>self)[preloadRef] as HTMLLinkElement | undefined;
+    let url = fallbackUrl;
+    if(preload !== undefined) {
+        url = preload.href;
+    }
     if(navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
         return  await doImport(url);
     }else{
