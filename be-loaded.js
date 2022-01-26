52313e29f21edda8cfce796c41716feb2a7ed8d4
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-01-26 03:22:44 -0500

    0.0.40

--- be-loaded.js
+++ be-loaded.js
@@ -7,14 +7,14 @@ export class BeLoadedController {
         console.log('intro', { proxy, target });
         this.#target = target;
         if (document.readyState === 'loading') {
-            console.log(target, 'readyState =' + document.readyState);
+            //console.log(target, 'readyState =' +  document.readyState)
             proxy.domLoading = true;
             document.addEventListener('DOMContentLoaded', e => {
-                console.log(target, 'DOMContentLoaded');
+                //console.log(target, 'DOMContentLoaded');
                 proxy.domLoading = false;
                 proxy.domLoaded = true;
                 if (proxy.needsRedoing) {
-                    console.log(target, 'needsRedoing');
+                    //console.log(target, 'needsRedoing');
                     if (proxy.stylesheets !== undefined) {
                         this.onStylesheets(this);
                     }
@@ -25,20 +25,20 @@ export class BeLoadedController {
             }, { once: true });
             return;
         }
-        console.log('setting domLoaded = true');
+        //console.log('setting domLoaded = true');
         proxy.domLoaded = true;
     }
     async onLoadParams({ fallback, preloadRef, proxy }) {
-        console.log({ fallback, preloadRef, proxy }, 'onLoadParams');
+        //console.log({fallback, preloadRef, proxy}, 'onLoadParams');
         const loadParams = { fallback, preloadRef };
         const stylesheet = await this.loadStylesheet(this, loadParams);
         if (stylesheet === true) {
             proxy.needsRedoing = true;
-            console.log('needs redoing', { fallback, preloadRef, proxy });
+            //console.log('needs redoing', {fallback, preloadRef, proxy});
             return; //need to wait
         }
         if (stylesheet === false) {
-            console.log('stylesheet is false', { fallback, preloadRef, proxy });
+            //console.log('stylesheet is false', {fallback, preloadRef, proxy});
             return;
         }
         const rn = proxy.getRootNode();
@@ -48,14 +48,14 @@ export class BeLoadedController {
         else {
             rn.adoptedStyleSheets = [stylesheet.default];
         }
-        console.log('stylesheet is loaded', { fallback, preloadRef, proxy });
+        //console.log('stylesheet is loaded', {fallback, preloadRef, proxy});
         this.doRemoveStyle(this, rn);
         // setTimeout(() => {
         //     this.doRemoveStyle(this, rn);
         // }, 20);
     }
     doRemoveStyle({ removeStyle, proxy }, rn) {
-        console.log('doRemoveStyle', { removeStyle, proxy });
+        //console.log('doRemoveStyle', {removeStyle, proxy});
         switch (typeof removeStyle) {
             case 'string':
                 {
@@ -71,14 +71,14 @@ export class BeLoadedController {
         }
     }
     async onStylesheets({ stylesheets, proxy, removeStyle: styleIdToRemove }) {
-        console.log('onStylesheets', { stylesheets, proxy, removeStyle: styleIdToRemove });
+        //console.log('onStylesheets', {stylesheets, proxy, removeStyle: styleIdToRemove});
         const adoptedStylesheets = [];
         const rn = proxy.getRootNode();
         for (const stylesheet of stylesheets) {
             const adoptedStylesheet = await this.loadStylesheet(this, stylesheet);
             if (adoptedStylesheet === true) {
                 proxy.needsRedoing = true;
-                console.log('needs redoing', { stylesheets, proxy, removeStyle: styleIdToRemove });
+                //console.log('needs redoing', {stylesheets, proxy, removeStyle: styleIdToRemove});
                 return; //need to wait
             }
             if (adoptedStylesheet === false)
@@ -92,21 +92,21 @@ export class BeLoadedController {
         }
         if (adoptedStylesheets.length > 0)
             rn.adoptedStyleSheets = adoptedStylesheets;
-        console.log('stylesheets are loaded', { stylesheets, proxy, removeStyle: styleIdToRemove });
+        //console.log('stylesheets are loaded', {stylesheets, proxy, removeStyle: styleIdToRemove});
         this.doRemoveStyle(this, rn);
     }
     async loadStylesheet({ proxy, domLoading }, { fallback, preloadRef }) {
-        console.log('loadStylesheet', { proxy, domLoading, fallback, preloadRef });
+        //console.log('loadStylesheet', {proxy, domLoading, fallback, preloadRef});
         if (preloadRef === undefined) {
             throw 'preloadRef is required';
         }
         const link = self[preloadRef];
         if (link !== undefined) {
-            console.log('link is defined', { proxy, domLoading, fallback, preloadRef });
+            //console.log('link is defined', {proxy, domLoading, fallback, preloadRef});
             return await importCSS(link.href);
         }
         else if (domLoading) {
-            console.log('link is undefined, domLoading still', { proxy, domLoading, fallback, preloadRef });
+            //console.log('link is undefined, domLoading still', {proxy, domLoading, fallback, preloadRef});
             return true;
         }
         if (fallback !== undefined) {
@@ -117,11 +117,11 @@ export class BeLoadedController {
             preloadLink.as = "script";
             preloadLink.crossOrigin = "anonymous";
             document.head.appendChild(preloadLink);
-            console.log('link is undefined, fallback is defined', { proxy, domLoading, fallback, preloadRef });
+            //console.log('link is undefined, fallback is defined', {proxy, domLoading, fallback, preloadRef});
             return await this.loadStylesheet(this, { fallback, preloadRef });
         }
         else {
-            console.log('link is undefined, fallback is undefined', { proxy, domLoading, fallback, preloadRef });
+            //console.log('link is undefined, fallback is undefined', {proxy, domLoading, fallback, preloadRef});
             return false;
         }
     }
--- be-loaded.ts
+++ be-loaded.ts
@@ -9,14 +9,14 @@ export class BeLoadedController implements BeLoadedActions{
         console.log('intro', {proxy, target});
         this.#target = target;
         if(document.readyState === 'loading'){
-            console.log(target, 'readyState =' +  document.readyState)
+            //console.log(target, 'readyState =' +  document.readyState)
             proxy.domLoading = true;
             document.addEventListener('DOMContentLoaded', e => {
-                console.log(target, 'DOMContentLoaded');
+                //console.log(target, 'DOMContentLoaded');
                 proxy.domLoading = false;
                 proxy.domLoaded = true;
                 if(proxy.needsRedoing){
-                    console.log(target, 'needsRedoing');
+                    //console.log(target, 'needsRedoing');
                     if(proxy.stylesheets !== undefined){
                         this.onStylesheets(this);
                     }else{
@@ -27,20 +27,20 @@ export class BeLoadedController implements BeLoadedActions{
             }, {once: true});
             return;
         }
-        console.log('setting domLoaded = true');
+        //console.log('setting domLoaded = true');
         proxy.domLoaded = true;
     }
     async onLoadParams({fallback, preloadRef,  proxy}: this) {
-        console.log({fallback, preloadRef, proxy}, 'onLoadParams');
+        //console.log({fallback, preloadRef, proxy}, 'onLoadParams');
         const loadParams: ILoadParams = {fallback, preloadRef}; 
         const stylesheet = await this.loadStylesheet(this, loadParams);
         if(stylesheet === true) {
             proxy.needsRedoing = true;
-            console.log('needs redoing', {fallback, preloadRef, proxy});
+            //console.log('needs redoing', {fallback, preloadRef, proxy});
             return; //need to wait
         }
         if(stylesheet === false) {
-            console.log('stylesheet is false', {fallback, preloadRef, proxy});
+            //console.log('stylesheet is false', {fallback, preloadRef, proxy});
             return;
         }
         const rn = proxy.getRootNode() as DocumentFragment;
@@ -49,14 +49,14 @@ export class BeLoadedController implements BeLoadedActions{
         }else{
             (rn as any).adoptedStyleSheets = [stylesheet.default];
         }
-        console.log('stylesheet is loaded', {fallback, preloadRef, proxy});
+        //console.log('stylesheet is loaded', {fallback, preloadRef, proxy});
         this.doRemoveStyle(this, rn);
         // setTimeout(() => {
         //     this.doRemoveStyle(this, rn);
         // }, 20);
     }
     doRemoveStyle({removeStyle, proxy}: this, rn: DocumentFragment){
-        console.log('doRemoveStyle', {removeStyle, proxy});
+        //console.log('doRemoveStyle', {removeStyle, proxy});
         switch(typeof removeStyle){
             case 'string':
                 {
@@ -71,14 +71,14 @@ export class BeLoadedController implements BeLoadedActions{
         }
     }
     async onStylesheets({stylesheets, proxy, removeStyle: styleIdToRemove}: this){
-        console.log('onStylesheets', {stylesheets, proxy, removeStyle: styleIdToRemove});
+        //console.log('onStylesheets', {stylesheets, proxy, removeStyle: styleIdToRemove});
         const adoptedStylesheets: StyleSheet[] = [];
         const rn = proxy.getRootNode() as DocumentFragment;
         for(const stylesheet of stylesheets){
             const adoptedStylesheet = await this.loadStylesheet(this, stylesheet);
             if(adoptedStylesheet === true) {
                 proxy.needsRedoing = true;
-                console.log('needs redoing', {stylesheets, proxy, removeStyle: styleIdToRemove});
+                //console.log('needs redoing', {stylesheets, proxy, removeStyle: styleIdToRemove});
                 return; //need to wait
             }
             if(adoptedStylesheet === false) continue;
@@ -90,21 +90,21 @@ export class BeLoadedController implements BeLoadedActions{
             
         }
         if(adoptedStylesheets.length > 0) (rn as any).adoptedStyleSheets = adoptedStylesheets;
-        console.log('stylesheets are loaded', {stylesheets, proxy, removeStyle: styleIdToRemove});
+        //console.log('stylesheets are loaded', {stylesheets, proxy, removeStyle: styleIdToRemove});
         this.doRemoveStyle(this, rn);
     }
 
     async loadStylesheet({proxy, domLoading}: this, {fallback, preloadRef}: ILoadParams) : Promise<boolean | StylesheetImport | HTMLLinkElement> {
-        console.log('loadStylesheet', {proxy, domLoading, fallback, preloadRef});
+        //console.log('loadStylesheet', {proxy, domLoading, fallback, preloadRef});
         if(preloadRef === undefined){
             throw 'preloadRef is required';
         }
         const link = (<any>self)[preloadRef] as HTMLLinkElement;
         if(link !== undefined){
-            console.log('link is defined', {proxy, domLoading, fallback, preloadRef});
+            //console.log('link is defined', {proxy, domLoading, fallback, preloadRef});
             return await importCSS(link.href!);
         }else if(domLoading){
-            console.log('link is undefined, domLoading still', {proxy, domLoading, fallback, preloadRef});
+            //console.log('link is undefined, domLoading still', {proxy, domLoading, fallback, preloadRef});
             return true;
         }
             
@@ -116,10 +116,10 @@ export class BeLoadedController implements BeLoadedActions{
             preloadLink.as = "script";
             preloadLink.crossOrigin = "anonymous";
             document.head.appendChild(preloadLink);
-            console.log('link is undefined, fallback is defined', {proxy, domLoading, fallback, preloadRef});
+            //console.log('link is undefined, fallback is defined', {proxy, domLoading, fallback, preloadRef});
             return await this.loadStylesheet(this, {fallback, preloadRef});
         }else{
-            console.log('link is undefined, fallback is undefined', {proxy, domLoading, fallback, preloadRef});
+            //console.log('link is undefined, fallback is undefined', {proxy, domLoading, fallback, preloadRef});
             return false;
         }
     }
